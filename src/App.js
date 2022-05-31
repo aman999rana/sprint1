f807ec64a12c8acd9935bcb277f264bfc6491cb0
aman999rana <rana333aman@gmail.com>
2022-05-31 12:13:52 +0530

    opp

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import { Opp }from './components/Opp'
+
+function App() {
+  return (
+    <div className="App">
+     <Opp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Opp.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+
+export const Opp=()=>{
+
+    return <div>
+        <h1>Mobile Operarting System</h1>
+  <div style={{width:"30%", margin:"auto"}}>
+          <ul>
+          <li>Androids</li>
+          <li>Blackberry</li>
+          <li>iPhone</li>
+          <li>Windows Phone</li>
+      </ul>
+  </div>
+  <h1>Mobile Manufacturer</h1>
+<div style={{width:"30%", margin:"auto"}}>
+        <ul>
+        <li>Samsung</li>
+        <li>HTC</li>
+        <li>Micromax</li>
+        <li>Apple</li>
+    </ul>
+</div>
+    
+
+
+    </div>
+}
